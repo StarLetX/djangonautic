bba89d1420b29b64971d67c0006b6a84c31f95dd
Rasheed A. Muhammed <starletgh@gmail.com>
2018-10-17 07:42:19 +0000

    New updates for Heroku deployment

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xd8)87arlo2hk&#$zw18iag)-(3xw9lna1%^rj*yabr+7nmx(*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
